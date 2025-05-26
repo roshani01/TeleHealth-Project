9af1a9b4689370b9719682443fe1cbc00403ccc8
roshani01 <roshanim075@gmail.com>
2025-05-27 00:01:24 +0530

    second

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,14 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Local dev only; Vercel uses its own port
-  },
-  build: {
-    outDir: 'dist', // Vercel expects a static folder like "dist"
-    
-  },
-});
+  plugins: [react()]
+})
